d1e9ce4e79071a6ba4e36a224c6d1854c7d9ee5e
Ajay Kumar Reddy Arram <77767099+ajayarram@users.noreply.github.com>
2021-04-19 23:42:52 -0500

    changes made to files

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -10,6 +10,10 @@ var cookieParser = require('cookie-parser');
 
 var logger = require('morgan');
 
+var passport = require('passport');
+
+var LocalStrategy = require('passport-local').Strategy;
+
 var connectionString = process.env.MONGO_CON;
 mongoose = require('mongoose');
 mongoose.connect(connectionString, {
@@ -41,13 +45,51 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
+passport.use(new LocalStrategy(function (username, password, done) {
+  Account.findOne({
+    username: username
+  }, function (err, user) {
+    if (err) {
+      return done(err);
+    }
+
+    if (!user) {
+      return done(null, false, {
+        message: 'Incorrect username.'
+      });
+    }
+
+    if (!user.validPassword(password)) {
+      return done(null, false, {
+        message: 'Incorrect password.'
+      });
+    }
+
+    return done(null, user);
+  });
+}));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express["static"](path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cow', cowRouter);
 app.use('/stars', starsRouter);
 app.use('/slot', slotRouter);
-app.use('/resource', resourceRouter); // catch 404 and forward to error handler
+app.use('/resource', resourceRouter); // passport config
+// Use the existing connection
+// The Account model
+
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser()); // catch 404 and forward to error handler
 
 app.use(function (req, res, next) {
   next(createError(404));
